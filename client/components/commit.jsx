7e0f33ef40b355d2423561199af425f03f686bb4
Uzair Ashraf <uzinatorcl@live.com>
2019-10-15 21:22:12 -0700

    deed data comes in properly on click of the category the bug was so obvious //kaetai

--- client/components/commit.jsx
+++ client/components/commit.jsx
@@ -7,46 +7,74 @@ class Commit extends React.Component {
     super(props);
     this.state = {
       categoryList: [],
+      deedList: [],
       view: 'categoryList',
-      fetchCompleted: false
+      categoryFetchCompleted: false,
+      categoryToDisplay: null
     };
+    this.changeCommitView = this.changeCommitView.bind(this);
+    this.getDeeds = this.getDeeds.bind(this);
   }
   componentDidMount() {
     fetch('api/categories.php')
       .then(response => response.json())
       .then(data => {
-        this.setState({ categoryList: data, fetchCompleted: true });
+        this.setState({ categoryList: data, categoryFetchCompleted: true });
+      })
+      .catch(error => console.error(error));
+  }
+  getDeeds(categoryId) {
+    fetch(`api/deeds.php?id=${categoryId}`)
+      .then(response => response.json())
+      .then(data => {
         console.log(data);
+        this.setState({ deedList: data });
       })
       .catch(error => console.error(error));
   }
   generateCategoryList() {
-    if (this.state.fetchCompleted === true) {
+    if (this.state.categoryFetchCompleted === true) {
       return this.state.categoryList.map(category => {
-        return <Categories key={category.category_id} id={category.category_id}name={category.name} />;
+        return <Categories key={category.category_id} id={category.category_id} name={category.name} changeView={this.changeCommitView} select={this.getDeeds}/>;
       });
     } else {
       return 'loading categories...';
     }
   }
-
-  render() {
+  changeCommitView(newView, newCategory) {
+    this.setState({ view: newView, categoryToDisplay: newCategory });
+  }
+  commitDisplay() {
     const display = this.generateCategoryList();
+    if (this.state.categoryToDisplay === null) {
+      return (
+        <>
+          <div className="heading">CHOOSE YOUR CATEGORY</div>
+          <div className="categoryList">
+            {display}
+          </div>
+        </>
+      );
+    } else {
+      return (
+        null
+      );
+    }
+  }
+  render() {
+    const display = this.commitDisplay();
     return (
       <div className="container">
-        <Header/>
-        <div className="heading">CHOOSE YOUR CATEGORY</div>
-        <div className="categoryList">
-          {display}
-        </div>
-        <Footer setView={this.props.setView}/>
+        <Header />
+        {display}
+        <Footer setView={this.props.setView} />
       </div>
     );
   }
 }
 function Categories(props) {
   return (
-    <div className="category">{props.name.toUpperCase()}</div>
+    <div className="category" onClick={() => { props.select(props.id); } }>{props.name.toUpperCase()}</div>
   );
 }
 
