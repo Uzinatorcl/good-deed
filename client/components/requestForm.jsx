9926880fe9d9c982f36ca075744044c21cd5f4f4
Uzair Ashraf <uzinatorcl@live.com>
2019-10-24 19:28:34 -0700

    user can now render zipcode using navigator object and google maps

--- client/components/requestForm.jsx
+++ client/components/requestForm.jsx
@@ -5,14 +5,38 @@ class RequestForm extends React.Component {
     super(props);
     this.state = {
       headline: '',
-      zipcode: '',
-      summary: ''
+      zipcode: null,
+      summary: '',
+      lat: null,
+      long: null,
+      gotUserLocation: false
     };
     this.trackHeadline = this.trackHeadline.bind(this);
     this.trackZipcode = this.trackZipcode.bind(this);
     this.trackSummary = this.trackSummary.bind(this);
     this.generateRequest = this.generateRequest.bind(this);
+    this.getUserLocation = this.getUserLocation.bind(this);
   }
+
+  getUserLocation(e) {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        lat: position.coords.latitude,
+        long: position.coords.longitude }, () => {
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.state.lat},${this.state.long}&key=AIzaSyA93u-gCVeeTT8nby7oIc2swhkQcPuBsDE`)
+          .then(response => response.json())
+          .then(data => {
+            const zipcode = data.results[0].address_components.find(result => {
+              return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(result.short_name);
+            }).short_name;
+            this.setState({ zipcode: zipcode, gotUserLocation: true });
+          });
+
+      });
+    });
+  }
+
   generateRequest() {
     const request = {
       'category_id': this.props.categoryId,
@@ -39,13 +63,14 @@ class RequestForm extends React.Component {
   }
 
   render() {
+    const currentLocationButton = this.state.gotUserLocation ? this.state.zipcode : 'GET CURRENT LOCATION';
     return (
       <div className="requestContainer">
         <div className="heading">REQUEST INFO</div>
         <div className="requestFormContainer">
           <form className="requestForm">
             <input type="text" maxLength="61" className="requestBoxHeadline" id="headline" placeholder="HEADLINE" onChange={this.trackHeadline} value={this.state.headline} />
-            <input type="text" maxLength="10" className="requestBoxZipcode" id="zipcode" placeholder="ZIPCODE" onChange={this.trackZipcode} value={this.state.zipcode} />
+            <button onClick={this.getUserLocation} className="requestBoxZipcode">{currentLocationButton}</button>
             <label className="summaryLabel">SUMMARY</label>
             <textarea id="summary" className="requestBoxSummary" cols="30" rows="8" onChange={this.trackSummary} value={ this.state.summary }></textarea>
           </form>
