28da5da73440cb9ff4b82f78b0ead86936e97a60
Uzair Ashraf <uzinatorcl@live.com>
2019-10-26 23:22:19 -0700

    user can officially use maps, back end sends back data properly now

--- server/public/api/get_deeds.php
+++ server/public/api/get_deeds.php
@@ -4,16 +4,16 @@ if (!defined('INTERNAL')) {
 }
 $cat_id = intval($_GET['catid']);
 $user_id = intval($_GET['id']);
+$locationDataRecieved = isset($_GET['lat']) && isset($_GET['long']);
 
+if($locationDataRecieved) {
 
-if(isset($_GET['latitude']) && isset($_GET['longitude'])) {
+  $latitude = floatval($_GET['lat']);
 
-  $latitude = floatval($_GET['latitude']);
-
-  $longitude = floatval($_GET['longitude']);
+  $longitude = floatval($_GET['long']);
 
   $query = "SELECT R.`request_id`, R.`category_id`, R.`headline`, R.`summary`, R.`request_user_id`, R.`completed`,R.`latitude`,R.`longitude`, U.`username`, U.`zipcode`, U.`image_url`,
-  CEIL((SELECT ST_Distance_Sphere(point(R.`longitude`, R.`latitude`), point(-117.56010889999999, 33.8403633)))*0.000621371192) < 30 AS near
+  CEIL((SELECT ST_Distance_Sphere(point(R.`longitude`, R.`latitude`), point($longitude, $latitude)))*0.000621371192) < 10 AS near
   FROM `requests` AS R
   JOIN `users` AS U
   ON U.`user_id` = R.`request_user_id`
@@ -32,9 +32,18 @@ if (!$result) {
   throw new Exception('Error'.mysqli_error($conn));
 }
 $deedList = [];
-while($row = mysqli_fetch_assoc($result)) {
-  array_push($deedList, $row);
+  if($locationDataRecieved) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    if($row['near']) {
+      array_push($deedList, $row);
+    }
+  }
+} else {
+  while ($row = mysqli_fetch_assoc($result)) {
+    array_push($deedList, $row);
+  }
 }
+
 
 $deedList = json_encode($deedList);
 
